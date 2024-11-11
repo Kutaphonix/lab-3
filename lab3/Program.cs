7006197d5a444244f7dbfa8e2d353c0d409e0ce1
Kutaphonix <shofertm@gmail.com>
2024-11-11 20:40:08 +0100

    2nd commit - dictionary,switch,methods

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -8,8 +8,63 @@ namespace lab3
 {
     internal class Program
     {
+        static Dictionary<string, string> dictionary = new Dictionary<string, string>()
+        {
+            {"kot", "cat"}, {"pies", "dog"}, {"horse", "koń"}, {"mouse", "mysz"}, {"frog", "żaba"}, {"wolf", "wilk"}, {"monkey", "małpa"},
+            {"fox", "lis"}, {"tiger", "tygrys"}, {"lion", "lew"}, {"elephant", "słoń"}, {"parrot", "papuga"}, {"pig", "świnia"}, {"sheep", "owca"},
+            {"cow", "krowa"}, {"chicken", "kurczak"}
+        };
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("English - Polish dictionary");
+                Console.WriteLine("[1] Translator");
+                Console.WriteLine("[2] See all words");
+                Console.WriteLine("[3] Add new word");
+                Console.WriteLine("[4] Delete word");
+                Console.WriteLine("[5] See translation history");
+                Console.WriteLine("[6] Test");
+                Console.WriteLine("[0] Exit");
+
+                string wybor = Console.ReadLine();
+
+                switch (wybor)
+                {
+                    case "1":
+                        Translator();
+                        break;
+                    case "2":
+                        SeeWords();
+                        break;
+                    case "3":
+                        AddWord();
+                        break;
+                    case "4":
+                        DeleteWord();
+                        break;
+                    case "5":
+                        SeeHistory();
+                        break;
+                    case "6":
+                        Test();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Wrong number. Press to continue");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
+        static void Translator() { }
+        static void SeeWords() { }
+        static void AddWord() { }
+        static void DeleteWord() { }
+        static void SeeHistory() { }
+        static void Test() { }
+
     }
 }
